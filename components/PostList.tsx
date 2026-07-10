e4e0c77d06ef5e7b750ec02b15b375700dbf1381
Tai Dev <tantailg2003@gmail.com>
2026-07-10 22:03:45 +0700

    update tim kiem caterogy

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -12,6 +12,27 @@ type PostListProps = {
 export default function PostList({ posts }: PostListProps) {
   const [keyword, setKeyword] = useState("");
 
+  const [selectedCategory, setSelectedCategory] = useState("Tất cả");
+
+   const categories = [
+    "Tất cả",
+    ...new Set(posts.map(post => post.category))
+    ];
+
+  const filteredPostsCategory = useMemo(() => {
+    return posts.filter(post => {
+        const matchTitle =
+        post.title.toLowerCase().includes(keyword.toLowerCase());
+
+        const matchCategory =
+        selectedCategory === "Tất cả" ||
+        post.category === selectedCategory;
+
+        return matchTitle && matchCategory;
+    });
+    }, [posts, keyword, selectedCategory]);
+
+
   const filteredPosts = useMemo(() => {
     const normalizedKeyword = keyword.trim().toLowerCase();
 
@@ -28,13 +49,34 @@ export default function PostList({ posts }: PostListProps) {
     <div className="max-w-6xl mx-auto px-4 py-8">
       <h1 className="text-4xl font-bold mb-6">Tất cả bài viết</h1>
 
-      <input
-        type="search"
-        placeholder="Tìm bài viết..."
-        value={keyword}
-        onChange={(e) => setKeyword(e.target.value)}
-        className="w-full border rounded-lg px-4 py-3 mb-8"
-      />
+        // Search input
+        <div className="max-w-xl mb-8">
+            <input
+                type="search"
+                placeholder="Tìm bài viết..."
+                value={keyword}
+                onChange={(e) => setKeyword(e.target.value)}
+                className="w-full border rounded-lg px-4 py-3"
+            />
+        </div>
+
+        // Category filter
+        <div className="flex gap-2 flex-wrap mb-6">
+        {categories.map(category => (
+            <button
+            key={category}
+            onClick={() => setSelectedCategory(category)}
+            className={`px-4 py-2 rounded-full ${
+                selectedCategory === category
+                ? "bg-blue-600 text-white"
+                : "bg-gray-200"
+            }`}
+            >
+            {category}
+            </button>
+        ))}
+        </div>
+
 
       {filteredPosts.length === 0 ? (
         <p className="text-gray-500">

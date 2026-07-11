e565c9672e39b7ddbfa33e29a50a72471d798c06
Tai Dev <tantailg2003@gmail.com>
2026-07-11 11:15:31 +0700

    whitelist supabase

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,23 @@
 import type { NextConfig } from "next";
 
-const nextConfig = {
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'picsum.photos',
+        protocol: "https",
+        hostname: "picsum.photos",
       },
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "vvhyysizqdlmmsfxzled.supabase.co",
+        pathname: "/storage/v1/object/public/game-images/**",
       },
     ],
   },
 };
+
 export default nextConfig;
